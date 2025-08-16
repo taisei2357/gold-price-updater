260780b15a869a1d7d50b0d5094edc6541c5807a
taisei <taisei19971021@gmail.com>
2025-08-16 18:30:29 +0900

    remove duplicate _index.tsx file

--- app/routes/_index.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@remix-run/node";
-
-export const loader = async () => redirect("/app");
-
-// 見せる要素は不要だが、型の都合で空を返しておく
-export default function Index() { 
-  return null; 
-}
